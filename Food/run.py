dcce49951dfac9c6643a97c5fc7bdd8e535d1913
barema4 <samrubarema6@gmail.com>
2018-10-02 08:03:34 -0700

    add run file

---
+++ Food/run.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from urls import GetRoutes
+from flask_jwt_extended import JWTManager
+app = Flask(__name__)
+app.env = 'development'
+app.testing = True
+GetRoutes.fetch_routes(app)
+app.config['JWT_SECRET_KEY'] = 'rubarema'
+jwt = JWTManager(app)
+if __name__ == '__main__':
+    app.run(debug=True)
