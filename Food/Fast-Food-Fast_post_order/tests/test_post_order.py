6b77641aee14c1fc2af8cc4aedf747334f69f522
barema4 <samrubarema6@gmail.com>
2018-10-03 23:55:49 -0700

    add post order files [#160882864]

---
+++ Food/Fast-Food-Fast_post_order/tests/test_post_order.py
@@ -0,0 +1,23 @@
+import unittest
+import json
+from run import app
+
+class UsersTest(unittest.TestCase):
+    """
+    Users Test Case
+    """
+
+    def setUp(self):
+        """
+        Test Setup
+        """
+        self.order = app
+        self.client = self.order.test_client
+
+    def test_post_order(self):
+        result = self.client().post('api/v2/orders', content_type="application/json", data=json.dumps(
+            dict(order_name="matoke", order_status="Decline",)))
+        self.assertEqual(result.status_code, 401)
+        resp = json.loads(result.data.decode())
+        self.assertIn('msg', resp)
+        self.assertTrue(resp["msg"])
